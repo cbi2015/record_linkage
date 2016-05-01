e665d9a7a11c27fbe0b9d19f12f1d8b66fc39954
cbi2015 <biyer@balaji-macbook-pro.local>
2016-05-01 19:46:41 -0400

    Updated file_name

--- go.bash
+++ go.bash
@@ -4,9 +4,9 @@ set -u
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p ${DIR}/logs
 
-python ${DIR}/product_listing.py -p products.txt -l listings.txt > go_result
+python ${DIR}/product_listing.py -p products.txt -l listings.txt > result_go
 
-result_file=`cat go_result`
+result_file=`cat result_go`
 
 result_output=`curl -XPOST -F file=@${result_file} https://challenge-check.sortable.com/validate`
 echo $result_output
